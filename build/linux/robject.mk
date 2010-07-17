df842001c9d1779254ae56edfc42a2e32196cfa8
mstoilov <mstoilov@b0bb84a5-424d-4f98-af44-3ef3f117eb03>
2010-07-16 23:42:51 -0700

    dev
    git-svn-id: svn+ssh://svn.crossrain.com/svn/rpase/trunk/rtk@139 b0bb84a5-424d-4f98-af44-3ef3f117eb03

--- build/linux/robject.mk
+++ build/linux/robject.mk
@@ -1,38 +1,14 @@
-ROBJECT_SRCDIR = $(SRCDIR)/robject
-ROBJECT_LIB = $(OUTDIR)/librobject.a
-ROBJECT_SO = $(OUTDIR)/librobject.so.1.0
-
-ROBJECT_OBJECTS =	\
-	$(OUTDIR)/robject.o \
-
-
-ifeq ($(OS), linux)
-all: $(OUTDIR) $(ROBJECT_LIB) $(ROBJECT_SO)
-else
-all: $(OUTDIR) $(ROBJECT_LIB)
-endif
-
-
-$(OUTDIR)/%.o: $(ROBJECT_SRCDIR)/%.c
-	$(CC) $(CFLAGS) -o $(OUTDIR)/$*.o -c $(ROBJECT_SRCDIR)/$*.c
-
-$(ROBJECT_LIB): $(ROBJECT_OBJECTS)
-	$(AR) -cr $@ $^
-
-$(ROBJECT_SO): $(ROBJECT_OBJECTS)
-	$(CC) $(LDFLAGS) -shared -Wl,-soname,librobject.so -o $@ $^
-
-$(OUTDIR):
-	@mkdir $(OUTDIR)
+all:
+	make -C $(SRCDIR)/rlib/build/$(OS)/$(ARCHDIR) all
+	make -C $(SRCDIR)/rvm/build/$(OS)/$(ARCHDIR) all
+	make -C $(SRCDIR)/tests/build/$(OS)/$(ARCHDIR) all
 
 distclean: clean
-	@rm -f .depend
-	@rm -rf $(OUTDIR)
+	make -C $(SRCDIR)/rlib/build/$(OS)/$(ARCHDIR) distclean
+	make -C $(SRCDIR)/rvm/build/$(OS)/$(ARCHDIR) distclean
+	make -C $(SRCDIR)/tests/build/$(OS)/$(ARCHDIR) distclean
 
 clean:
-	@rm -f $(ROBJECT_LIB)
-	@rm -f $(ROBJECT_SO)
-	@rm -f $(ROBJECT_OBJECTS)
-	@rm -f *~
-	@rm -f $(SRCDIR)/*~
-
+	make -C $(SRCDIR)/rlib/build/$(OS)/$(ARCHDIR) clean
+	make -C $(SRCDIR)/rvm/build/$(OS)/$(ARCHDIR) clean
+	make -C $(SRCDIR)/tests/build/$(OS)/$(ARCHDIR) clean
