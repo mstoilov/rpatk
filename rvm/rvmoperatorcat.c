f4858aab358e7ccc298a3067ea125bf9e3ce6c15
mstoilov <mstoilov@b0bb84a5-424d-4f98-af44-3ef3f117eb03>
2010-10-02 00:04:47 -0700

    dev
    git-svn-id: svn+ssh://svn.crossrain.com/svn/rpase/trunk/rtk@198 b0bb84a5-424d-4f98-af44-3ef3f117eb03

---
+++ rvm/rvmoperatorcat.c
@@ -0,0 +1,15 @@
+#include "rvmoperatorcat.h"
+#include "rstring.h"
+
+
+void rvm_op_cat_string_string(rvm_cpu_t *cpu, rvm_reg_t *res, const rvm_reg_t *arg1, const rvm_reg_t *arg2)
+{
+	rstring_t *s1 = (rstring_t*)arg1->v.p;
+	rstring_t *s2 = (rstring_t*)arg2->v.p;
+	rstring_t *dst;
+
+	dst = r_string_create_from_rstr(&s1->s);
+	r_string_cat(dst, &s2->s);
+	RVM_REG_UNREF(res);
+	RVM_REG_ASSIGN_STRING(res, dst);
+}
---
+++ rvm/rvmoperatorcat.h
@@ -0,0 +1,8 @@
+#ifndef _RVMOPERATORCAT_H_
+#define _RVMOPERATORCAT_H_
+
+#include "rvmoperator.h"
+
+void rvm_op_cat_string_string(rvm_cpu_t *cpu, rvm_reg_t *res, const rvm_reg_t *arg1, const rvm_reg_t *arg2);
+
+#endif
