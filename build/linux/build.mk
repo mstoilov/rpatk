1a2c122f82a4dff9c22f3d706b66976e8d707837
Martin Stoilov <martin@rpasearch.com>
2010-11-16 21:44:35 -0800

    Fix the build CFLAGS

--- build/linux/build.mk
+++ build/linux/build.mk
@@ -1,17 +1,17 @@
 all:
-	make -C $(SRCDIR)/rlib/build/$(OS)/$(ARCHDIR) all
-	make -C $(SRCDIR)/rpa/build/$(OS)/$(ARCHDIR) all
-	make -C $(SRCDIR)/rvm/build/$(OS)/$(ARCHDIR) all
-	make -C $(SRCDIR)/tests/build/$(OS)/$(ARCHDIR) all
+	+make -C $(SRCDIR)/rlib/build/$(OS)/$(ARCHDIR) all
+	+make -C $(SRCDIR)/rpa/build/$(OS)/$(ARCHDIR) all
+	+make -C $(SRCDIR)/rvm/build/$(OS)/$(ARCHDIR) all
+	+make -C $(SRCDIR)/tests/build/$(OS)/$(ARCHDIR) all
 
 distclean: clean
-	make -C $(SRCDIR)/rlib/build/$(OS)/$(ARCHDIR) distclean
-	make -C $(SRCDIR)/rpa/build/$(OS)/$(ARCHDIR) distclean
-	make -C $(SRCDIR)/rvm/build/$(OS)/$(ARCHDIR) distclean
-	make -C $(SRCDIR)/tests/build/$(OS)/$(ARCHDIR) distclean
+	+make -C $(SRCDIR)/rlib/build/$(OS)/$(ARCHDIR) distclean
+	+make -C $(SRCDIR)/rpa/build/$(OS)/$(ARCHDIR) distclean
+	+make -C $(SRCDIR)/rvm/build/$(OS)/$(ARCHDIR) distclean
+	+make -C $(SRCDIR)/tests/build/$(OS)/$(ARCHDIR) distclean
 
 clean:
-	make -C $(SRCDIR)/rlib/build/$(OS)/$(ARCHDIR) clean
-	make -C $(SRCDIR)/rpa/build/$(OS)/$(ARCHDIR) clean
-	make -C $(SRCDIR)/rvm/build/$(OS)/$(ARCHDIR) clean
-	make -C $(SRCDIR)/tests/build/$(OS)/$(ARCHDIR) clean
+	+make -C $(SRCDIR)/rlib/build/$(OS)/$(ARCHDIR) clean
+	+make -C $(SRCDIR)/rpa/build/$(OS)/$(ARCHDIR) clean
+	+make -C $(SRCDIR)/rvm/build/$(OS)/$(ARCHDIR) clean
+	+make -C $(SRCDIR)/tests/build/$(OS)/$(ARCHDIR) clean
