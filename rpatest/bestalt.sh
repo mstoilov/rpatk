2969e0fed74a91623fc20930726d10297b1ddeaa
Martin Stoilov <martin@rpasearch.com>
2011-01-01 17:08:55 -0800

    added bestalt test

---
+++ rpatest/bestalt.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ -f common.def ]; then
+. common.def
+fi
+
+RPATEST=bestalt
+DIFF="diff -u"
+
+if [ "" = "" ]; then
+RPAGREP=../rgrep/unix/x86_64/rgrep
+fi
+
+
+$RPAGREP -p -f $RPATEST.rpa -c ".*" $RPATEST.in > $RPATEST.res
+$DIFF $RPATEST.res $RPATEST.out
+
+if [ $? != 0 ]; then
+    echo "$RPATEST : FAILED"
+else
+    echo "$RPATEST : PASSED"
+fi
+
