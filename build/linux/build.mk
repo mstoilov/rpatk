39f68ec13d2f1da795fbb94b19a18956d722bc3b
Martin Stoilov <martin@rpasearch.com>
2011-05-15 16:51:50 -0700

    cleanup - 3

--- build/linux/build.mk
+++ build/linux/build.mk
@@ -3,9 +3,9 @@ all:
 	+make -C $(SRCDIR)/rpa2/build/$(OS)/$(ARCHDIR) all
 	+make -C $(SRCDIR)/rvm/build/$(OS)/$(ARCHDIR) all
 	+make -C $(SRCDIR)/rjs/build/$(OS)/$(ARCHDIR) all
-	+make -C $(SRCDIR)/tests/build/$(OS)/$(ARCHDIR) all
-	+make -C $(SRCDIR)/testrpa2/build/$(OS)/$(ARCHDIR) all
-	+make -C $(SRCDIR)/testrjs/build/$(OS)/$(ARCHDIR) all
+	+make -C $(SRCDIR)/tests/testmisc/build/$(OS)/$(ARCHDIR) all
+	+make -C $(SRCDIR)/tests/testrpa2/build/$(OS)/$(ARCHDIR) all
+	+make -C $(SRCDIR)/tests/testrjs/build/$(OS)/$(ARCHDIR) all
 	+make -C $(SRCDIR)/rgrep/build/$(OS)/$(ARCHDIR) all
 
 distclean: clean
@@ -13,9 +13,9 @@ distclean: clean
 	+make -C $(SRCDIR)/rpa2/build/$(OS)/$(ARCHDIR) distclean
 	+make -C $(SRCDIR)/rvm/build/$(OS)/$(ARCHDIR) distclean
 	+make -C $(SRCDIR)/rjs/build/$(OS)/$(ARCHDIR) distclean
-	+make -C $(SRCDIR)/tests/build/$(OS)/$(ARCHDIR) distclean
-	+make -C $(SRCDIR)/testrpa2/build/$(OS)/$(ARCHDIR) distclean
-	+make -C $(SRCDIR)/testrjs/build/$(OS)/$(ARCHDIR) distclean
+	+make -C $(SRCDIR)/tests/testmisc/build/$(OS)/$(ARCHDIR) distclean
+	+make -C $(SRCDIR)/tests/testrpa2/build/$(OS)/$(ARCHDIR) distclean
+	+make -C $(SRCDIR)/tests/testrjs/build/$(OS)/$(ARCHDIR) distclean
 	+make -C $(SRCDIR)/rgrep/build/$(OS)/$(ARCHDIR) distclean
 
 clean:
@@ -23,8 +23,8 @@ clean:
 	+make -C $(SRCDIR)/rpa2/build/$(OS)/$(ARCHDIR) clean
 	+make -C $(SRCDIR)/rvm/build/$(OS)/$(ARCHDIR) clean
 	+make -C $(SRCDIR)/rjs/build/$(OS)/$(ARCHDIR) clean
-	+make -C $(SRCDIR)/tests/build/$(OS)/$(ARCHDIR) clean
-	+make -C $(SRCDIR)/testrpa2/build/$(OS)/$(ARCHDIR) clean
-	+make -C $(SRCDIR)/testrjs/build/$(OS)/$(ARCHDIR) clean
+	+make -C $(SRCDIR)/tests/testmisc/build/$(OS)/$(ARCHDIR) clean
+	+make -C $(SRCDIR)/tests/testrpa2/build/$(OS)/$(ARCHDIR) clean
+	+make -C $(SRCDIR)/tests/testrjs/build/$(OS)/$(ARCHDIR) clean
 	+make -C $(SRCDIR)/rgrep/build/$(OS)/$(ARCHDIR) clean
 
