6856556d6cc88a93ac803d31ccded851075e904e
Martin Stoilov <martin@rpasearch.com>
2011-05-15 16:26:12 -0700

    cleanup - 2

--- build/linux/build.mk
+++ build/linux/build.mk
@@ -1,6 +1,5 @@
 all:
 	+make -C $(SRCDIR)/rlib/build/$(OS)/$(ARCHDIR) all
-	+make -C $(SRCDIR)/rast/build/$(OS)/$(ARCHDIR) all
 	+make -C $(SRCDIR)/rpa2/build/$(OS)/$(ARCHDIR) all
 	+make -C $(SRCDIR)/rvm/build/$(OS)/$(ARCHDIR) all
 	+make -C $(SRCDIR)/rjs/build/$(OS)/$(ARCHDIR) all
@@ -11,7 +10,6 @@ all:
 
 distclean: clean
 	+make -C $(SRCDIR)/rlib/build/$(OS)/$(ARCHDIR) distclean
-	+make -C $(SRCDIR)/rast/build/$(OS)/$(ARCHDIR) distclean
 	+make -C $(SRCDIR)/rpa2/build/$(OS)/$(ARCHDIR) distclean
 	+make -C $(SRCDIR)/rvm/build/$(OS)/$(ARCHDIR) distclean
 	+make -C $(SRCDIR)/rjs/build/$(OS)/$(ARCHDIR) distclean
@@ -22,7 +20,6 @@ distclean: clean
 
 clean:
 	+make -C $(SRCDIR)/rlib/build/$(OS)/$(ARCHDIR) clean
-	+make -C $(SRCDIR)/rast/build/$(OS)/$(ARCHDIR) clean
 	+make -C $(SRCDIR)/rpa2/build/$(OS)/$(ARCHDIR) clean
 	+make -C $(SRCDIR)/rvm/build/$(OS)/$(ARCHDIR) clean
 	+make -C $(SRCDIR)/rjs/build/$(OS)/$(ARCHDIR) clean
