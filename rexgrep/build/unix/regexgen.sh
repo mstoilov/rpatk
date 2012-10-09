398ef7e8d3703362111a044eda37a5960d5851e1
Martin Stoilov <martin@rpasearch.com>
2012-10-08 23:11:33 -0700

    Added LICENSE and README

--- rexgrep/build/unix/regexgen.sh
+++ rexgrep/build/unix/regexgen.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-
 for i in {a..z} {A..Z} {0..9}; do 
     for j in {a..z} {A..Z} {0..9}; do 
-	for k in {a..b}; do
-	    echo -n "${i}?${j}?${k}?"
-	    echo -n 'X5O\!P\%\@AP\[4\\PZX54\(P\^\)7CC\)7\}\$[E-F][H-I][B-D]AR\-STA[M-O]DARD\-'
-	    echo -n "${j}[N-P]TI(V${i}RU[R-T]|NOV[I-K]R[T-V]S?)"
-	    echo '\-TEST\-FILE\!\$H\+H\*|'
+	for k in {U..X}; do
+	    echo "${i}${j}O\!P\%\@AP\[4\\\\PZX54\(P\\^\\)7CC\\)7\}\\\$E[I-J]CAR-[S-V]TANDARD-[A-E]NTIVIR${k}S-TEST-FILE\\!\\\$H\\+H\\* | "
 	done
     done
 done
-
-echo -n "[mn]a[qr]t[i-j]*n|"
+echo -n "[mn]a[qr]t[i-j]*n[ ]*[r-t]to[i-j]l[m-p][v-w]|"
 echo "theregexend"; 
